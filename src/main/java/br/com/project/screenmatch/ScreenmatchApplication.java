4f5aae1353636afc9c7034b165328744eeddff2c
Mythalie Sandretti <s.mythalie@hotmail.com>
2024-01-31 17:06:00 -0300

    Criando menu de interação e buscando dados da série

--- src/main/java/br/com/project/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/project/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.project.screenmatch;
 
-import br.com.project.screenmatch.model.EpisodeData;
-import br.com.project.screenmatch.model.SeasonData;
-import br.com.project.screenmatch.model.ShowData;
-import br.com.project.screenmatch.service.ApiConsumptionService;
-import br.com.project.screenmatch.service.ConvertDataService;
+import br.com.project.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,24 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var apiConsumption = new ApiConsumptionService();
-		ConvertDataService convert = new ConvertDataService();
-
-		var showJson = apiConsumption.getData("https://www.omdbapi.com/?t=game+of+thrones&apikey=6585022c");
-		ShowData showData = convert.getData(showJson, ShowData.class);
-		System.out.println(showData);
-
-		var episodeJson = apiConsumption.getData("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=6585022c");
-		EpisodeData episodeData = convert.getData(episodeJson, EpisodeData.class);
-		System.out.println(episodeData);
-
-		List<SeasonData> seasons = new ArrayList<>();
-
-		for (int i = 1; i <= showData.totalSeasons(); i++) {
-			var seasonJson = apiConsumption.getData("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i + "&apikey=6585022c");
-			SeasonData seasonData = convert.getData(seasonJson, SeasonData.class);
-			seasons.add(seasonData);
-		}
-		seasons.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.displayMenu();
 	}
 }
---
+++ src/main/java/br/com/project/screenmatch/principal/Principal.java
@@ -0,0 +1,58 @@
+package br.com.project.screenmatch.principal;
+
+import br.com.project.screenmatch.model.SeasonData;
+import br.com.project.screenmatch.model.ShowData;
+import br.com.project.screenmatch.service.ApiConsumptionService;
+import br.com.project.screenmatch.service.ConvertDataService;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+
+    private Scanner reading = new Scanner(System.in);
+    private ApiConsumptionService apiConsumption = new ApiConsumptionService();
+    private ConvertDataService convert = new ConvertDataService();
+    private final String ADDRESS = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void displayMenu() {
+        while (true) {
+            System.out.println("Digite o nome da série: ");
+            var seriesName = reading.nextLine();
+
+            try {
+                // Substitui espaços em branco por "+"
+                String encodedSeriesName = URLEncoder.encode(seriesName.replace(" ", "+"), StandardCharsets.UTF_8.toString());
+
+                var showJson = apiConsumption.getData(ADDRESS + encodedSeriesName + API_KEY);
+                ShowData showData = convert.getData(showJson, ShowData.class);
+
+                // Verifica se o objeto ShowData e totalSeasons não são nulos
+                if (showData != null && showData.totalSeasons() != null) {
+                    System.out.println(showData);
+
+                    List<SeasonData> seasons = new ArrayList<>();
+
+                    for (int i = 1; i <= showData.totalSeasons().intValue(); i++) {
+                        var seasonJson = apiConsumption.getData(ADDRESS + encodedSeriesName + "&season=" + i + API_KEY);
+                        SeasonData seasonData = convert.getData(seasonJson, SeasonData.class);
+                        seasons.add(seasonData);
+                    }
+                    seasons.forEach(System.out::println);
+
+                    // Se o nome da série foi encontrado e as condições foram satisfeitas, sai do loop
+                    break;
+                } else {
+                    System.out.println("Série não encontrada ou informações incompletas. Por favor, digite um nome de série existente.");
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
