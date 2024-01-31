d34562c4b27a82a3396ee1fb72ca93cf3b0ae171
Mythalie Sandretti <s.mythalie@hotmail.com>
2024-01-31 15:59:05 -0300

    Modelando temporadas

--- src/main/java/br/com/project/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/project/screenmatch/ScreenmatchApplication.java
@@ -1,12 +1,16 @@
 package br.com.project.screenmatch;
 
 import br.com.project.screenmatch.model.EpisodeData;
+import br.com.project.screenmatch.model.SeasonData;
 import br.com.project.screenmatch.model.ShowData;
 import br.com.project.screenmatch.service.ApiConsumptionService;
 import br.com.project.screenmatch.service.ConvertDataService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -27,5 +31,14 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var episodeJson = apiConsumption.getData("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=6585022c");
 		EpisodeData episodeData = convert.getData(episodeJson, EpisodeData.class);
 		System.out.println(episodeData);
+
+		List<SeasonData> seasons = new ArrayList<>();
+
+		for (int i = 1; i <= showData.totalSeasons(); i++) {
+			var seasonJson = apiConsumption.getData("https://www.omdbapi.com/?t=game+of+thrones&Season=" + i + "&apikey=6585022c");
+			SeasonData seasonData = convert.getData(seasonJson, SeasonData.class);
+			seasons.add(seasonData);
+		}
+		seasons.forEach(System.out::println);
 	}
 }
---
+++ src/main/java/br/com/project/screenmatch/model/SeasonData.java
@@ -0,0 +1,10 @@
+package br.com.project.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SeasonData(@JsonAlias("Season") Integer number, @JsonAlias("Episodes") List<EpisodeData> episodes) {
+}
