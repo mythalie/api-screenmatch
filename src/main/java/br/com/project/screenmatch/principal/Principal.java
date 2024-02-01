67d9ab637c3fccc312ed6e03b7e62cc12f1a3449
Mythalie Sandretti <s.mythalie@hotmail.com>
2024-02-01 11:18:27 -0300

    Criando uma nova classe para lidar com os dados do episódio e melhoria no código

--- src/main/java/br/com/project/screenmatch/principal/Principal.java
+++ src/main/java/br/com/project/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.project.screenmatch.principal;
 
+import br.com.project.screenmatch.model.Episode;
 import br.com.project.screenmatch.model.EpisodeData;
 import br.com.project.screenmatch.model.SeasonData;
 import br.com.project.screenmatch.model.ShowData;
@@ -17,51 +18,31 @@ import java.util.stream.Collectors;
 
 public class Principal {
 
+    private static final String ADDRESS = "https://www.omdbapi.com/?t=";
+    private static final String API_KEY = "&apikey=6585022c";
+
     private Scanner reading = new Scanner(System.in);
     private ApiConsumptionService apiConsumption = new ApiConsumptionService();
     private ConvertDataService convert = new ConvertDataService();
-    private final String ADDRESS = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=6585022c";
 
+    /*Método displayMenu:
+        Este método representa o menu principal do programa, onde o usuário é solicitado a digitar o nome da série.
+        O nome da série é então codificado utilizando o método encodeSeriesName.
+        A chamada ao método getShowData obtém os dados da série da API.
+        Se os dados da série e o número total de temporadas não são nulos, o método displayShowInfo é chamado. Caso contrário, uma mensagem de erro é exibida.*/
     public void displayMenu() {
         while (true) {
             System.out.println("Digite o nome da série: ");
             var seriesName = reading.nextLine();
 
             try {
-                // Substitui espaços em branco por "+"
-                String encodedSeriesName = URLEncoder.encode(seriesName.replace(" ", "+"), StandardCharsets.UTF_8.toString());
+                String encodedSeriesName = encodeSeriesName(seriesName);
 
-                var showJson = apiConsumption.getData(ADDRESS + encodedSeriesName + API_KEY);
-                ShowData showData = convert.getData(showJson, ShowData.class);
+                ShowData showData = getShowData(encodedSeriesName);
 
                 // Verifica se o objeto ShowData e totalSeasons não são nulos
                 if (showData != null && showData.totalSeasons() != null) {
-                    System.out.println(showData);
-
-                    List<SeasonData> seasons = new ArrayList<>();
-
-                    for (int i = 1; i <= showData.totalSeasons().intValue(); i++) {
-                        var seasonJson = apiConsumption.getData(ADDRESS + encodedSeriesName + "&season=" + i + API_KEY);
-                        SeasonData seasonData = convert.getData(seasonJson, SeasonData.class);
-                        seasons.add(seasonData);
-                    }
-                    //seasons.forEach(System.out::println);
-
-                    seasons.forEach(t -> t.episodes().forEach(e -> System.out.println(e.title())));
-
-                    List<EpisodeData> episodeData = seasons.stream()
-                                    .flatMap(t -> t.episodes().stream())
-                                    .collect(Collectors.toList());
-
-                    System.out.println("\nTop 5 episódios");
-                    episodeData.stream()
-                            .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
-                            .sorted(Comparator.comparing(EpisodeData::rating).reversed())
-                            .limit(5)
-                            .forEach(System.out::println);
-
-                    // Se o nome da série foi encontrado e as condições foram satisfeitas, sai do loop
+                    displayShowInfo(showData, encodedSeriesName);
                     break;
                 } else {
                     System.out.println("Série não encontrada ou informações incompletas. Por favor, digite um nome de série existente.");
@@ -70,5 +51,57 @@ public class Principal {
                 e.printStackTrace();
             }
         }
+    }
+
+    /*Método encodeSeriesName:
+        Este método substitui espaços em branco por "+" no nome da série e o codifica.*/
+    private String encodeSeriesName(String seriesName) throws UnsupportedEncodingException {
+        return URLEncoder.encode(seriesName.replace(" ", "+"), StandardCharsets.UTF_8.toString());
+    }
+
+    /*Método getShowData:
+        Este método faz uma chamada à API usando a URL construída com o nome da série e a chave da API.
+        Em seguida, converte a resposta JSON para um objeto ShowData usando o serviço de conversão.*/
+    private ShowData getShowData(String encodedSeriesName) {
+        String showJson = apiConsumption.getData(ADDRESS + encodedSeriesName + API_KEY);
+        return convert.getData(showJson, ShowData.class);
+    }
+
+    /*Método displayShowInfo:
+        Este método exibe informações sobre a série, como detalhes gerais e informações sobre cada temporada.*/
+    private void displayShowInfo(ShowData showData, String encodedSeriesName) {
+        System.out.println(showData);
+
+        List<SeasonData> seasons = new ArrayList<>();
+
+        for (int i = 1; i <= showData.totalSeasons().intValue(); i++) {
+            var seasonJson = apiConsumption.getData(ADDRESS + encodedSeriesName + "&season=" + i + API_KEY);
+            SeasonData seasonData = convert.getData(seasonJson, SeasonData.class);
+            seasons.add(seasonData);
+        }
+        displayEpisodes(seasons);
+    }
+
+    /*Método displayEpisodes:
+        Este método exibe os cinco melhores episódios em termos de classificação e, em seguida, exibe todas as temporadas e episódios.*/
+    private void displayEpisodes(List<SeasonData> seasons) {
+        List<EpisodeData> episodeData = seasons.stream()
+                .flatMap(t -> t.episodes().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios");
+        episodeData.stream()
+                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(EpisodeData::rating).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        System.out.println();
+        List<Episode> episodes = seasons.stream()
+                .flatMap(t -> t.episodes().stream()
+                        .map(d -> new Episode(t.number(), d))
+                ).collect(Collectors.toList());
+
+        episodes.forEach(System.out::println);
     }
 }
