649661e28dd7cb91c193e52ea65ba6d41215f6ac
Mythalie Sandretti <s.mythalie@hotmail.com>
2024-02-09 18:21:49 -0300

    Adicionando comentário

--- src/main/java/br/com/project/screenmatch/controller/FavoriteSeriesController.java
+++ src/main/java/br/com/project/screenmatch/controller/FavoriteSeriesController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -36,7 +37,16 @@ public class FavoriteSeriesController {
         return ResponseEntity.ok(titleUpdate);
     }
 
+    /*
+    Outra maneira de restringir o acesso a determinadas funcionalidades, com base no perfil dos usuários, é com a utilização de um recurso
+    do Spring Security conhecido como Method Security.
+    A anotação @Secured("ROLE_ADMIN"), apenas usuários com o perfil ADMIN possam disparar requisições para deletar.
+    A anotação @Secured pode ser adicionada em métodos individuais ou mesmo na classe, que seria o equivalente a adicioná-la em todos os métodos.
+    Por padrão esse recurso vem desabilitado no spring Security, sendo que para o utilizar devemos adicionar a seguinte anotação na
+    classe Securityconfigurations do projeto: @EnableMethodSecurity(securedEnabled = true)
+     */
     @DeleteMapping("{id}")
+    // @Secured("ROLE_ADMIN")
     public ResponseEntity delete(@PathVariable Long id) {
         ResponseBase<FavoriteSeriesResponse> remove = service.delete(id);
         return ResponseEntity.ok(remove);
