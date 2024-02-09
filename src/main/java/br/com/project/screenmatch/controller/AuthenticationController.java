e73856d01922f970184f0977950a5d23cb5c26c8
Mythalie Sandretti <s.mythalie@hotmail.com>
2024-02-09 16:41:29 -0300

    Adicionando JSON Web Token

--- src/main/java/br/com/project/screenmatch/controller/AuthenticationController.java
+++ src/main/java/br/com/project/screenmatch/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package br.com.project.screenmatch.controller;
 
 import br.com.project.screenmatch.domain.dto.AuthenticationCreateRequest;
+import br.com.project.screenmatch.domain.entity.User;
+import br.com.project.screenmatch.domain.dto.DataTokenJWT;
+import br.com.project.screenmatch.service.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +21,17 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity logIn(@RequestBody @Valid AuthenticationCreateRequest createRequest) {
-        var token = new UsernamePasswordAuthenticationToken(createRequest.login(), createRequest.password());
-        var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var authenticationToken = new UsernamePasswordAuthenticationToken(createRequest.login(), createRequest.password());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.createToken((User) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DataTokenJWT(tokenJWT));
     }
 
 }
---
+++ src/main/java/br/com/project/screenmatch/domain/dto/DataTokenJWT.java
@@ -0,0 +1,4 @@
+package br.com.project.screenmatch.domain.dto;
+
+public record DataTokenJWT(String token){
+}
